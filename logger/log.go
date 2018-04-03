fb98f2f04b89f16f62ca05e7f5653e355ff0685c
Joseph Blodgett <joseph.blodgett@gmail.com>
2018-04-03 16:09:06 -0600

    initial commit

---
+++ logger/log.go
@@ -0,0 +1,27 @@
+package logger
+
+import (
+	"log"
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var L *zap.SugaredLogger
+var cfg zap.Config
+
+func init() {
+
+	cfg = zap.NewDevelopmentConfig()
+	cfg.OutputPaths = append(cfg.OutputPaths, os.Getenv("./testing.log"))
+	cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	l, err := cfg.Build()
+	if err != nil {
+		log.Printf("Couldn't build config for zap logger: %v", err.Error())
+		panic(err)
+	}
+	L = l.Sugar()
+	L.Info("Zap Logger Started")
+
+}
---
+++ logger/options.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/byuoitav/av-api/base"
+
+func Init() {
+
+}
+
+func GenerateBody(device string) (rm base.PublicRoom) {
+	rm.Displays := []base.Device{}
+
+
+
+}
